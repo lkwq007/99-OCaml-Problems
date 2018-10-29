9d843d6fc40a0021940c1e27449987977a317634
Death <lkwq007@gmail.com>
2018-10-29 20:29:32 +0800

    Change some expressions

--- arithmetic/p34.ml
+++ arithmetic/p34.ml
@@ -5,6 +5,6 @@ let phi num=
       if b=0 then a else aux b (a mod b)
     in (aux a b)=1
   in let rec judge m n count=
-       if n<m then begin if coprime m n then judge m (n+1) (count+1) else judge m (n+1) count end
+       if n<=m then begin if coprime m n then judge m (n+1) (count+1) else judge m (n+1) count end
        else count
   in judge num 1 0;;
