89a76ba4cab7b490740e75ac3b9fa4a37cf37e5e
Death <lkwq007@gmail.com>
2018-10-29 16:17:52 +0800

    ocp-indent those files

--- working-with-lists/p01.ml
+++ working-with-lists/p01.ml
@@ -1,6 +1,6 @@
 (* Write a function last : 'a list -> 'a option that returns the last element of a list. *)
 let rec last lst = 
-    match lst with
-    | [] -> None
-    | head::[] -> Some head
-    | head::tail -> last tail;;
+  match lst with
+  | [] -> None
+  | head::[] -> Some head
+  | head::tail -> last tail;;
--- working-with-lists/p18.ml
+++ working-with-lists/p18.ml
@@ -1,6 +1,6 @@
 (* Extract a slice from a list. *)
 let slice lst lower upper=
-    let rec select n low up=function
-        | [] -> []
-        | hd::tl -> if n>=low && n<=up then hd::select (n+1) low up tl else select (n+1) low up tl
-    in select 0 lower upper lst;;
+  let rec select n low up=function
+    | [] -> []
+    | hd::tl -> if n>=low && n<=up then hd::select (n+1) low up tl else select (n+1) low up tl
+  in select 0 lower upper lst;;
